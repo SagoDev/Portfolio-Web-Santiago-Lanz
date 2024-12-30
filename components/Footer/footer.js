eea5dbc235eb320117df23842fab933dc80886a3
Santiago Lanz <133175709+SagoDev@users.noreply.github.com>
2024-12-30 20:35:38 -0300

    Componente Footer
    - Creada estrucutura base para footer.

---
+++ components/Footer/footer.js
@@ -0,0 +1,10 @@
+fetch('/components/Footer/footer.html')
+    .then(response => response.text())
+    .then(html => document.getElementById('footer').innerHTML = html);
+
+const footerCSS = document.createElement('link');
+footerCSS.rel = 'stylesheet'
+footerCSS.href = '/components/Footer/footer.style.css'
+
+document.head.appendChild(footerCSS);
+document.getElementById('footer').innerHTML = navbarHTML;
