5f6424a41a0e8cc31aab500f20a13e862a0e72c0
Santiago Lanz <133175709+SagoDev@users.noreply.github.com>
2025-02-19 10:54:18 -0300

    Data Analyst - Sección Sobre Mi
    - Estilo de Slidebar  terminado.

--- pages/dataAnalyst/dataAnalyst.js
+++ pages/dataAnalyst/dataAnalyst.js
@@ -1,4 +1,5 @@
 let links = document.querySelectorAll('header nav a');
+let slidebarOptions = document.querySelectorAll('.ad-about .personal-dashboard .dashboard-slidebar .slidebar-body .slidebar-options button');
 
 links.forEach((link => {
     link.addEventListener('click', async (e) => {
@@ -6,6 +7,15 @@ links.forEach((link => {
 
         removeActive(links);
         link.classList.add('active');
+    });
+}));
+
+slidebarOptions.forEach((option => {
+    option.addEventListener('click', async (e) => {
+        e.preventDefault();
+
+        removeActive(slidebarOptions);
+        option.classList.add('active');
     });
 }));
 
